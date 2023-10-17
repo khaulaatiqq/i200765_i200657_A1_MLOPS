d0053e973631c2d053e526b603cbcc37715ecc27
khaulaatiqq <144622297+khaulaatiqq@users.noreply.github.com>
2023-10-17 18:03:18 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ app = Flask(__name__)
 with open('iris_classifier.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
 
-# Define a route to render the HTML form
 
+# Define a route to render the HTML form
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Define a route to handle form submission and make predictions
 
+# Define a route to handle form submission and make predictions
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
@@ -24,8 +24,6 @@ def predict():
         sepal_width = float(request.form['sepal_width'])
         petal_length = float(request.form['petal_length'])
         petal_width = float(request.form['petal_width'])
-
-        # Make a prediction using the loaded model
 
         features = [[sepal_length, sepal_width, petal_length, petal_width]]
         scaler = StandardScaler()
@@ -39,6 +37,6 @@ def predict():
         print(f"Exception: {e}")
         return render_template('index.html', error_message=str(e))
 
-if __name__ == '__main__':
 
-    app.run(debug=True,host='0.0.0.0' , port=8080)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
