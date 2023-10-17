93d903d6d717dd4bb521671eae5644a1d670cbf7
khaulaatiqq <144622297+khaulaatiqq@users.noreply.github.com>
2023-10-17 17:57:49 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,12 @@
 import pickle
 from flask import Flask, render_template, request
-import torch
-import torchvision
-from torchvision import transforms
-from PIL import Image
-import numpy as np
-
-import torch
-from torchvision import models
+from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 
 # Load the trained model from the pickle file
-model = models.alexnet(pretrained = True)  # Replace 'YourModel' with your actual model architecture
-model.load_state_dict(torch.load("your_model.pkl"))
-model.eval()
-
-# Define transformations for incoming images
-transform = transforms.Compose([
-    transforms.Resize((64, 64)),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
+with open('iris_classifier.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
 
 # Define a route to render the HTML form
 @app.route('/')
@@ -32,26 +17,27 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Get the uploaded image from the form
-        uploaded_image = request.files['image']
-        
-        if uploaded_image:
-            image = Image.open(uploaded_image)
-            image = transform(image)
-            image = image.unsqueeze(0)  # Add a batch dimension
+        # Get feature values from the form
+        sepal_length = float(request.form['sepal_length'])
+        sepal_width = float(request.form['sepal_width'])
+        petal_length = float(request.form['petal_length'])
+        petal_width = float(request.form['petal_width'])
 
-            with torch.no_grad():
-                outputs = model(image)
-                _, prediction = torch.max(outputs, 1)
-                class_index = prediction.item()
-                classes = ['Pebbles', 'Shells']
-                predicted_class = classes[class_index]
-                
-            return render_template('index.html', prediction=predicted_class)
+        print(f"Input Values: sepal_length={sepal_length}, sepal_width={sepal_width}, petal_length={petal_length}, petal_width={petal_width}")
+        # Make a prediction using the loaded model
+
+
+        features = [[sepal_length, sepal_width, petal_length, petal_width]]
+        scaler = StandardScaler()
+        features = scaler.fit_transform(features)
+
+        prediction = model.predict(features)[0]
+
+        return render_template('index.html', prediction=prediction)
 
     except Exception as e:
         print(f"Exception: {e}")
         return render_template('index.html', error_message=str(e))
 
-if __name__ == '__main':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port=8080)
